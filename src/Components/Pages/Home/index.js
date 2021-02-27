fd7c8a531dfd2da5bf47a1783b00299025ace2aa
JamesCostello-dev <jcotab@gmail.com>
2021-02-26 19:10:12 -0700

    rework tree and start Nav logic

---
+++ src/Components/Pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nobis
+        voluptates ex provident quis iure exercitationem, assumenda voluptate
+        aperiam iste non necessitatibus rem placeat nihil voluptatem quod
+        corrupti doloremque quia?
+      </p>
+    </div>
+  );
+};
+
+export default Home;
