402416d48faefc38f4778abadcaaf61ea36899bf
JamesCostello-dev <jcotab@gmail.com>
2021-02-27 00:06:49 -0700

    add bootstrap and create card

--- src/Components/Pages/About/index.js
+++ src/Components/Pages/About/index.js
@@ -1,15 +1,18 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const About = () => {
   return (
     <div>
-      <h1>James Costello</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
-        aspernatur, dolorum quas iste, dolores cumque vitae dolor provident odit
-        blanditiis quidem fugiat, cupiditate voluptate repellat ipsam saepe
-        tempora hic itaque.
-      </p>
+      <h1>About</h1>
+      <Card>
+        <Card.Body>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est
+          aspernatur, dolorum quas iste, dolores cumque vitae dolor provident
+          odit blanditiis quidem fugiat, cupiditate voluptate repellat ipsam
+          saepe tempora hic itaque.
+        </Card.Body>
+      </Card>
     </div>
   );
 };
--- src/Components/Pages/Home/index.js
+++ src/Components/Pages/Home/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>James Costello</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nobis
         voluptates ex provident quis iure exercitationem, assumenda voluptate
